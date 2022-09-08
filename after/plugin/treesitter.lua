79561c47d57677c0a7219a7d87ed5c4f3046c820
Yassine Zeriouh <y@zeriouh.io>
2022-09-09 01:22:48 +0200

    Add autotag for automatically closing tags

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -4,4 +4,7 @@ require('nvim-treesitter.configs').setup({
   highlight = {
     enable = true,
   },
+  autotag = {
+    enable = true,
+  },
 })
--- lua/xyassini/packer.lua
+++ lua/xyassini/packer.lua
@@ -45,6 +45,7 @@ return require("packer").startup(function(use)
     run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
   })
   use("jiangmiao/auto-pairs")
+  use("windwp/nvim-ts-autotag")
 
   -- Git
   use("kdheepak/lazygit.nvim")
