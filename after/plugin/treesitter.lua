9bcd61dbb34a4b3d164c7954c2a464b2b8df02f3
Yassine Zeriouh <y@zeriouh.io>
2022-09-12 03:34:26 +0200

    Add svelte to treesitter's ensure_installed

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,11 +1,11 @@
 require('nvim-treesitter.configs').setup({
-  ensure_installed = { "lua", "css", "html", "javascript", "json", "ruby", "typescript", "yaml", "astro" },
+  ensure_installed = { "lua", "css", "html", "javascript", "json", "ruby", "typescript", "yaml", "astro", "svelte" },
   auto_install = true,
   highlight = {
     enable = true,
   },
   autotag = {
     enable = true,
-    filetypes = { "html", "javascript", "javascriptreact", "typescriptreact", "svelte", "vue", "astro", "eruby" },
+    filetypes = { "html", "javascript", "javascriptreact", "typescriptreact", "svelte", "vue", "astro", "eruby", "svelte" },
   },
 })
