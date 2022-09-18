cd96d766c35e06d2da2d0ab7f28ec0c4ee8217f8
Yassine Zeriouh <y@zeriouh.io>
2022-09-18 06:16:37 +0200

    Change cmp sources order

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -1,31 +1,30 @@
 local cmp = require("cmp")
 local luasnip = require("luasnip")
 
-local select_opts = {behavior = cmp.SelectBehavior.Select}
+local select_opts = { behavior = cmp.SelectBehavior.Select }
 
 cmp.setup({
   snippet = {
     expand = function(args)
       require("luasnip").lsp_expand(args.body)
-    end
+    end,
   },
 
   sources = cmp.config.sources({
-    { name = "copilot" },
-    { name = "nvim_lsp", keyword_length = 3 },
+    { name = "nvim_lsp", keyword_length = 1 },
     { name = "path", keyword_length = 2 },
+  }, {
+    { name = "buffer" },
+    { name = "copilot" },
     { name = "luasnip", keyword_length = 2 },
-    { name = "nvim_lsp_document_symbol", keyword_length = 3 }
-  },{
-    { name = "buffer" }
   }),
 
   window = {
-    documentation = cmp.config.window.bordered()
+    documentation = cmp.config.window.bordered(),
   },
 
   formatting = {
-    fields = {'menu', 'abbr', 'kind'},
+    fields = { "menu", "abbr", "kind" },
     format = function(entry, vim_item)
       vim_item.menu = ({
         nvim_lsp = "",
@@ -35,17 +34,17 @@ cmp.setup({
         copilot = "ﮧ",
       })[entry.source.name]
       return vim_item
-    end
+    end,
   },
 
   mapping = {
-    ['<Up>'] = cmp.mapping.select_prev_item(select_opts),
-    ['<Down>'] = cmp.mapping.select_next_item(select_opts),
-    ['<Left>'] = cmp.mapping.scroll_docs(-4),
-    ['<Right>'] = cmp.mapping.scroll_docs(4),
+    ["<Up>"] = cmp.mapping.select_prev_item(select_opts),
+    ["<Down>"] = cmp.mapping.select_next_item(select_opts),
+    ["<Left>"] = cmp.mapping.scroll_docs(-4),
+    ["<Right>"] = cmp.mapping.scroll_docs(4),
 
-    ['<ESC>'] = cmp.mapping.close(),
-    ['<Tab>'] = cmp.mapping(function(fallback)
+    ["<ESC>"] = cmp.mapping.close(),
+    ["<Tab>"] = cmp.mapping(function(fallback)
       -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
       if cmp.visible() then
         local entry = cmp.get_selected_entry()
@@ -57,7 +56,6 @@ cmp.setup({
       else
         fallback()
       end
-    end, {"i","s","c",}),
-  }
+    end, { "i", "s", "c" }),
+  },
 })
-
