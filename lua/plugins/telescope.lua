24699d4bac281e34cdda51be6996090e09a58226
Yassine Zeriouh <y@zeriouh.io>
2023-06-02 19:41:39 +0200

    Use native fzf for telescape

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,18 +1,19 @@
 local status, telescope = pcall(require, "telescope")
-if (not status) then
+if not status then
   return
 end
 
 telescope.setup({
   file_ignore_patterns = { "node_modules/", "node%_modules/", ".git/", ".vscode/" },
   defaults = {
-    layout_strategy = 'horizontal',
+    layout_strategy = "horizontal",
     layout_config = {
       horizontal = {
         width = 0.9,
-        height = 0.99
-      }
-    }
+        height = 0.99,
+      },
+    },
   },
 })
 
+telescope.load_extension("fzf")
