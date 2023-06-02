154f2d9dd73234629ae1311eb1cac5aac2d7593e
Yassine Zeriouh <y@zeriouh.io>
2023-06-02 22:13:05 +0200

    Add lsp-install script to install LSP servers on system

---
+++ lsp-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Install system dependencies
+
+npm i -g @angular/language-server vscode-langservers-extracted emmet-ls prisma @tailwindcss/language-server typescript typescript-language-server markdownlint graphql-language-service-cli @astrojs/language-server bash-language-server
+brew install lua-language-server yamllint fzf shellcheck
