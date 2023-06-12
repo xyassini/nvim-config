34f0629acd461e71f2c9724f9a18811a11284ea2
Yassine Zeriouh <yassine.zeriouh@limehome.de>
2023-06-12 14:26:20 +0200

    Delete redundant lsp-install script

--- lsp-install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Install system dependencies
-
-npm i -g @angular/language-server vscode-langservers-extracted emmet-ls @prisma/language-server @tailwindcss/language-server typescript typescript-language-server markdownlint graphql-language-service-cli @astrojs/language-server bash-language-server
-brew install lua-language-server yamllint fzf shellcheck
