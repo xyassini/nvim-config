9df3fcfbc5b38a9b6334e731c057d487ca4445bc
Yassine Zeriouh <yassine.zeriouh@limehome.de>
2023-06-15 15:35:32 +0200

    Add early-retirement plugin 👴🏽

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -363,6 +363,13 @@ return {
       "nvim-telescope/telescope.nvim"
     }
   },
+  {
+    "chrisgrieser/nvim-early-retirement",
+    event = "VeryLazy",
+    config = function()
+      require("plugins.early-retirement")
+    end,
+  },
 
 
   ----------------------------------------
